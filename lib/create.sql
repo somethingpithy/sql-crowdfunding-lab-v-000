6ce31f407ee3ceac5a3da52ef05d131cade1f3dc
Carlota Bindner <cev.bindner@gmail.com>
2018-03-15 12:41:14 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,17 +6,17 @@ CREATE TABLE projects (
   funding_goal INTEGER,
   start_date TEXT,
   end_date TEXT
-)
+);
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
-)
+);
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
   amount INTEGER,
   user_id INTEGER,
   project_id INTEGER
-)
+);
