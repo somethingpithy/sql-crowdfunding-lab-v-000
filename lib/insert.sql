5aff07ca339a960823fbae2185c11c9751b1dbb8
Carlota Bindner <cev.bindner@gmail.com>
2018-03-15 12:51:39 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,62 +1,62 @@
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
-INSERT INTO users
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES  
+INSERT INTO users (id, name, age) VALUES  
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES 
 
-INSERT INTO projects
-INSERT INTO projects
-INSERT INTO projects
-INSERT INTO projects
-INSERT INTO projects
-INSERT INTO projects
-INSERT INTO projects
-INSERT INTO projects
-INSERT INTO projects
-INSERT INTO projects
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
 
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
-INSERT INTO pledges
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES  
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
